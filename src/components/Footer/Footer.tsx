9def8fb273ec357998468b164be4cff5985e70f5
Jose Alejandro <jose-a-mendez@hotmail.com>
2023-08-28 10:49:49 -0300

    Add function to active visibility the button Salir

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,18 +4,27 @@ import Button from "../Button";
 import * as SC from "./Footer.styles";
 import { RootState } from "../../store";
 import { setButton } from "../../reducers/buttonReducer";
+import { useLocation } from "react-router-dom";
 
 const Footer = () => {
+  // Consulta el estado global para obtener valor del estado del button
   const { isVisible } = useSelector((state: RootState) => state.button);
   const dispatch = useDispatch();
+  // Obtener url de la vista actual
+  const { pathname } = useLocation();
+  // Comparar si la url obtenida es diferente de la ruta raíz y establecer la variable en true
+  const isRoot = pathname !== RoutesEnum.ROOT ? true : false;
+  // Con esta variable obtenemos el valor de la variable global o en su defecto el valor de la variable isRoot
+  const buttonIsVisible = isVisible || isRoot;
 
+  // Manejo del estado global del button Salir
   const handleToggleButton = () => {
     dispatch(setButton(false));
   };
 
   return (
     <SC.Wrapper>
-      {isVisible && (
+      {buttonIsVisible && (
         <Button linkTo={RoutesEnum.ROOT} action={handleToggleButton}>
           Salir
         </Button>
